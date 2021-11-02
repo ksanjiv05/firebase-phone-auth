a42a6ad26c8438ed21789965385ae9b8e94f984a
sanjiv kumar pandit <ksanjiv05>
2021-11-03 00:31:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import firebaseConfig from "./config";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPhoneNumber,
+  RecaptchaVerifier,
+} from "firebase/auth";
 
 function App() {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const [number, setNumber] = React.useState("");
+  const [otp, setOtp] = React.useState("");
+  const [confirmation, setConfirmation] = React.useState(null);
+
+  const sendOtp = () => {
+    const recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {},
+      auth
+    );
+    signInWithPhoneNumber(auth, number, recaptchaVerifier)
+      .then((confirmation) => {
+        setConfirmation(confirmation);
+        alert("Otp send");
+      })
+      .catch((err) => {
+        console.error("otp sending failed", err);
+        alert("Otp sendig failed");
+      });
+  };
+
+  const varifyOtp = () => {
+    confirmation
+      .confirm(otp)
+      .then((result) => {
+        console.log(result);
+        alert("Your are authenticated");
+      })
+      .catch((error) => {
+        console.error("otp varification failed", error);
+        alert("invalid otp");
+      });
+  };
+  if (auth.currentUser) return <h1>You are now authenticated</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        paddingTop: "20%",
+      }}
+    >
+      {confirmation ? (
+        <input
+          type="text"
+          placeholder="enter otp"
+          onChange={(ev) => setOtp(ev.target.value)}
+        />
+      ) : (
+        <input
+          type="text"
+          placeholder="enter phone number"
+          onChange={(ev) => setNumber(ev.target.value)}
+        />
+      )}
+
+      <br />
+      <br />
+      <button
+        type="button"
+        onClick={() => (confirmation ? varifyOtp() : sendOtp())}
+      >
+        {" "}
+        {confirmation ? "varify otp" : "send otp"}
+      </button>
+      <div
+        id="recaptcha-container"
+        style={{
+          position: "absolute",
+          right: 0,
+          bottom: 0,
+        }}
+      ></div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYLj7X26p8tzIs6qCddlevjFeSJzMd7p0",
+  authDomain: "phone-auth-demo-4be11.firebaseapp.com",
+  projectId: "phone-auth-demo-4be11",
+  storageBucket: "phone-auth-demo-4be11.appspot.com",
+  messagingSenderId: "471678879310",
+  appId: "1:471678879310:web:b033c4895e54ba2f16651f",
+};
+
+export default firebaseConfig;
